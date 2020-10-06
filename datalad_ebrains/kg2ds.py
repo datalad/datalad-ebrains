d20cc00f10ada93785a5688d4df74b4abde1e879
Michael Hanke <michael.hanke@gmail.com>
2020-10-06 11:50:00 +0200

    Basic sketch of feeding a KG query result into `addurls`

--- datalad_ebrains/kg2ds.py
+++ datalad_ebrains/kg2ds.py
@@ -1,68 +1,135 @@
-from os.path import curdir
-from os.path import abspath
+from tempfile import NamedTemporaryFile
 
-from datalad.interface.base import Interface
-from datalad.interface.base import build_doc
-from datalad.support.param import Parameter
 from datalad.distribution.dataset import datasetmethod
-from datalad.interface.utils import eval_results
-from datalad.support.constraints import EnsureChoice
-
+from datalad.interface.base import (
+    Interface,
+    build_doc,
+)
 from datalad.interface.results import get_status_dict
+from datalad.interface.utils import eval_results
+from datalad.support.param import Parameter
+from datalad.support.constraints import (
+    Constraint,
+    EnsureNone,
+)
+from datalad.distribution.dataset import (
+    EnsureDataset,
+    require_dataset,
+)
+from datalad.support.json_py import (
+    jsondump,
+    jsonload,
+)
 
 
-# decoration auto-generates standard help
+class EnsureUUID(Constraint):
+    """Ensure that input is a valid UUID.
+    """
+    def __call__(self, value):
+        from uuid import UUID
+        return str(UUID(value))
+
+    def short_description(self):
+        return 'UUID'
+
+    def long_description(self):
+        return "value must be a valid UUID"
+
+
 @build_doc
-# all commands must be derived from Interface
 class KnowledgeGraph2Dataset(Interface):
-    # first docstring line is used a short description in the cmdline help
-    # the rest is put in the verbose help and manpage
     """Export any dataset from the EBRAINS Knowledge Graph as a DataLad dataset
 
-    Long description of arbitrary volume.
+    some
     """
 
-    # parameters of the command, must be exhaustive
     _params_ = dict(
-        # name of the parameter, must match argument name
-        language=Parameter(
-            # cmdline argument definitions, incl aliases
-            args=("-l", "--language"),
-            # documentation
-            doc="""language to say "hello" in""",
-            # type checkers, constraint definition is automatically
-            # added to the docstring
-            constraints=EnsureChoice('en', 'de')),
+        kgid=Parameter(
+            args=("kgid",),
+            metavar='KGID',
+            doc="""ID of a dataset in the knowledge graph. This is a UUID
+            that is the trailing part of the URL when looking at a dataset
+            at http://kg,ebrains.eu""",
+            ),
+            #constraints=EnsureUUID()),
+        dataset=Parameter(
+            args=("-d", "--dataset"),
+            doc=""""Dataset to create""",
+            constraints=EnsureDataset() | EnsureNone()
+        ),
     )
 
     @staticmethod
-    # decorator binds the command to the Dataset class as a method
     @datasetmethod(name='ebrains_kg2ds')
-    # generic handling of command results (logging, rendering, filtering, ...)
     @eval_results
-    # signature must match parameter list above
-    # additional generic arguments are added by decorators
-    def __call__(language='en'):
-        if language == 'en':
-            msg = 'Hello!'
-        elif language == 'de':
-            msg = 'Tachchen!'
-        else:
-            msg = ("unknown language: '%s'", language)
+    def __call__(kgid, dataset=None):
 
-        # commands should be implemented as generators and should
-        # report any results by yielding status dictionaries
-        yield get_status_dict(
-            # an action label must be defined, the command name make a good
-            # default
-            action='demo',
-            # most results will be about something associated with a dataset
-            # (component), reported paths MUST be absolute
-            path=abspath(curdir),
-            # status labels are used to identify how a result will be reported
-            # and can be used for filtering
-            status='ok' if language in ('en', 'de') else 'error',
-            # arbitrary result message, can be a str or tuple. in the latter
-            # case string expansion with arguments is delayed until the
-            # message actually needs to be rendered (analog to exception messages)
-            message=msg)
+        # TODO later we will perform an actual query here, but for now load
+        # a prefetched and stored response from a file
+        response = jsonload(open(kgid))
+        results = response.get('results', [])
+        if not results:
+            yield get_status_dict(
+                action='kg2ds',
+                status='impossible',
+                message=('knowledge graph query for dataset ID %s yielded no records',
+                         kgid),
+            )
+            return
+        if len(results) > 1:
+            yield get_status_dict(
+                action='kg2ds',
+                status='error',
+                message='knowledge graph query yielded more than one result record',
+            )
+            return
+        file_specs = results[0].get('https://schema.hbp.eu/myQuery/v1.0.0', [])
+        if not results:
+            yield get_status_dict(
+                action='kg2ds',
+                status='impossible',
+                message=('knowledge graph query for dataset ID %s yielded no files',
+                         kgid),
+            )
+            return
+
+        # construct a file list (with metadata) suitable for addurls
+        file_list = []
+        for spec in file_specs:
+            url_spec = {}
+            for s, d in (('https://schema.hbp.eu/myQuery/relativeUrl', 'kg_id'),
+                         ('https://schema.hbp.eu/myQuery/last_modified', 'last_modified'),
+                         ('https://schema.hbp.eu/myQuery/absolute_path', 'url'),
+                         ('https://schema.hbp.eu/myQuery/relative_path', 'path')):
+                if s in spec:
+                    url_spec[d] = spec[s]
+            if url_spec:
+                file_list.append(url_spec)
+            else:
+                pass
+                # TODO log skipped item
+
+        # TODO URLs cannot be accessed as such, but need prior authorization
+        # figure out how that must be done
+        with NamedTemporaryFile('w') as tmp_listfile:
+            jsondump(file_list, tmp_listfile)
+            tmp_listfile.seek(0)
+            from datalad.api import addurls
+            addurls(
+                dataset=dataset or '',
+                urlfile=tmp_listfile.name,
+                urlformat='{url}',
+                filenameformat='{path}',
+                input_type='json',
+                # TODO
+                fast=True,
+            )
+
+        # TODO? adjust timestamps to the last modification
+
+        yield None
+        #yield get_status_dict(
+        #    action='kg2ds',
+        #    path=abspath(curdir),
+        #    status='ok')
+
