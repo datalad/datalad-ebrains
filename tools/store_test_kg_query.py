801588861139eac48984059dc8e81ed1defceeac
Michael Hanke <michael.hanke@gmail.com>
2022-03-07 09:19:52 +0100

    Update query storage helper to token and new query base

--- tools/store_test_kg_query.py
+++ tools/store_test_kg_query.py
@@ -4,43 +4,37 @@ import json
 from os import environ
 from uuid import UUID
 
-# get this from https://nexus-iam.humanbrainproject.org/v0/oauth2/authorize
-if 'EBRAINS_TOKEN' not in environ:
-    raise RuntimeError(
-        'Provide access token via EBRAINS_TOKEN environment variable')
+from datalad_ebrains.kg_query import (
+    get_auth_headers,
+    get_token,
+)
 
-authheaders = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer {}'.format(environ['EBRAINS_TOKEN'])
-}
 
 # use query endpoint for datasets
-query_base = "https://kg.humanbrainproject.eu/query/minds/core/dataset/v1.0.0"
+query_base = 'https://core.kg.ebrains.eu/v3-beta/queries'
 
 
-def store_query(name, spec):
+def store_query(name, token, spec):
     url = "{}/{}".format(query_base, name)
     r = requests.put(
         url,
         data=spec,
-        headers=authheaders,
+        headers=get_auth_headers(token),
     )
     if r.ok:
         print('Successfully stored the query at %s ' % r.url)
     else:
         print('Problem with "put" protocol on url: %s ' % r.url )
         print(r)
+        print(r.reason)
+        print(r.content)
 
 
-def query_dataset(name, dataset_id):
-    url = "{}/{}/instances?databaseScope=RELEASED&datasetId={}".format(
-        query_base,
-        name,
-        dataset_id,
-    )
+def query_dataset(name, token, dataset_id):
+    url = "{}/{}/instances?stage=RELEASED&dataset_id={}".format(query_base, name, dataset_id)
     r = requests.get(
         url,
-        headers=authheaders,
+        headers=get_auth_headers(token),
     )
     if not r.ok:
         print('Problem with "get" protocol on url: %s ' % r.url )
@@ -59,6 +53,8 @@ if __name__ == "__main__":
                 argv[0]))
         exit(1)
 
+    token = get_token('ebrains')
+
     query_name = argv[1]
     try:
         dataset_id = UUID(argv[2])
@@ -66,8 +62,8 @@ if __name__ == "__main__":
         dataset_id = None
 
     if dataset_id is None:
-        store_query(query_name, open(argv[2], 'r'))
+        store_query(query_name, token, open(argv[2], 'r'))
     else:
-        results = query_dataset(query_name, dataset_id)
+        results = query_dataset(query_name, token, dataset_id)
         if results:
             print(json.dumps(results))
